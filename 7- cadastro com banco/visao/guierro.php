521b5c6192b7671317718787abbe03c66c7b1c66
epcgrs <emmanuelf988@gmail.com>
2018-04-11 16:08:16 -0300

    html5 reference doctype

--- 7- cadastro com banco/visao/guierro.php
+++ 7- cadastro com banco/visao/guierro.php
@@ -1,7 +1,7 @@
 <?php
-	session_start();
+session_start();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <!--
 Design by Free CSS Templates
 http://www.freecsstemplates.org
@@ -13,79 +13,79 @@ Version    : 1.0
 Released   : 20120915
 
 -->
-<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/modelo.dwt" codeOutsideHTMLIsLocked="false" -->
+<html lang="pt-br">
 <head>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Erro</title>
-<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
-<link href="../estilos/style.css" rel="stylesheet" type="text/css" media="screen" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Erro</title>
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+	<link href="../estilos/style.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
-<div id="wrapper">
-	<div id="header-wrapper">
-		<div id="header" class="container">
-			<div id="logo">
-				<h1><a href="#">Site exemplo </a></h1>
-			</div>
-			<div id="menu">
-				<ul>
-					<li class="current_page_item"><a href="../index.html">Homepage</a></li>
-					<li><a href="guicadusuario.html">cadastro</a></li>
-					
-                    <li><a href="../controle/usuariocontrole.php?op=consultar">consulta</a></li>
-				</ul>
-			</div>
-		</div>
-		<div id="banner">
-			<div class="content"><img src="../imagens/img02.jpg" width="1000" height="300" alt="" /></div>
-		</div>
-	</div>
-	<!-- end #header -->
-	
-	<div id="page">
-		<div id="content">
-        
-		  <div class="post">
-				<!-- InstanceBeginEditable name="conteúdo" -->
-<h2 class="title">Erro</h2>
-
-<p>
-	<?php
-		if( isset($_SESSION['e']) ){
-			$erros = array();
-			$erros = unserialize($_SESSION['e']);
-			
-			foreach($erros as $e){
-				echo '<br />'.$e;
-			}
-		}//fecha if
-	?>
-</p>
-				<!-- InstanceEndEditable -->
-
-			</div>
-		</div>
-		<!-- end #content -->
-		<div id="sidebar">
-			<ul>
-				<li>
-					<h2>Categories</h2>
+	<div id="wrapper">
+		<div id="header-wrapper">
+			<div id="header" class="container">
+				<div id="logo">
+					<h1><a href="#">Site exemplo </a></h1>
+				</div>
+				<div id="menu">
 					<ul>
-						<li><a href="#">link 1</a></li>
-						<li><a href="#">link 2</a></li>
-						<li><a href="#">link 3</a></li>
+						<li class="current_page_item"><a href="../index.html">Homepage</a></li>
+						<li><a href="guicadusuario.html">cadastro</a></li>
+
+						<li><a href="../controle/usuariocontrole.php?op=consultar">consulta</a></li>
 					</ul>
-				</li>
-                
-			</ul>
+				</div>
+			</div>
+			<div id="banner">
+				<div class="content"><img src="../imagens/img02.jpg" width="1000" height="300" alt="" /></div>
+			</div>
 		</div>
-		<!-- end #sidebar -->
-		<div style="clear: both;">&nbsp;</div>
-	</div>
-	<!-- end #page --> 
+		<!-- end #header -->
+
+		<div id="page">
+			<div id="content">
+
+				<div class="post">
+					<!-- InstanceBeginEditable name="conteúdo" -->
+					<h2 class="title">Erro</h2>
+
+					<p>
+						<?php
+						if( isset($_SESSION['e']) ){
+							$erros = array();
+							$erros = unserialize($_SESSION['e']);
+
+							foreach($erros as $e){
+								echo '<br />'.$e;
+							}
+		}//fecha if
+		?>
+	</p>
+	<!-- InstanceEndEditable -->
+
+</div>
+</div>
+<!-- end #content -->
+<div id="sidebar">
+	<ul>
+		<li>
+			<h2>Categories</h2>
+			<ul>
+				<li><a href="#">link 1</a></li>
+				<li><a href="#">link 2</a></li>
+				<li><a href="#">link 3</a></li>
+			</ul>
+		</li>
+
+	</ul>
+</div>
+<!-- end #sidebar -->
+<div style="clear: both;">&nbsp;</div>
+</div>
+<!-- end #page --> 
 </div>
 <div id="footer">
 	<p>Copyright (c) 2012 Sitename.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org">FCT</a>. Photos by <a href="http://fotogrph.com/">Fotogrph</a>.</p>
